8c17c5f3dc4ef69b24194fb1f77c464d4705bf52
EricInacio01 <ericinacio163@gmail.com>
2025-06-19 12:37:24 -0300

    add modular structure with ISSP support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,5 +14,11 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "PlayIntegrityFix"
-include(":app")
+rootProject.name = "PlayIntegrityFix-NEXT"
+include(":app")                   // Módulo principal Android
+include(":core")                  // Módulo com lógica e utilitários compartilhados
+include(":feature:auth")          // Módulo da funcionalidade de autenticação
+include(":feature:dashboard")     // Módulo da funcionalidade de painel/visualização
+
+project(":feature:auth").projectDir = file("feature/auth")
+project(":feature:dashboard").projectDir = file("feature/dashboard")
