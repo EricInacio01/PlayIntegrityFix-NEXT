40d7771738c4bbc8b2900a9ddc82c80883130e56
EricInacio01 <ericinacio163@gmail.com>
2025-06-19 12:38:00 -0300

    add modular structure with ISSP support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,10 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "PlayIntegrityFix-NEXT"
-include(":app")                   // Módulo principal Android
-include(":core")                  // Módulo com lógica e utilitários compartilhados
-include(":feature:auth")          // Módulo da funcionalidade de autenticação
-include(":feature:dashboard")     // Módulo da funcionalidade de painel/visualização
+include(":app")
+include(":core")                  
+include(":feature:auth")          
+include(":feature:dashboard")     
 
 project(":feature:auth").projectDir = file("feature/auth")
 project(":feature:dashboard").projectDir = file("feature/dashboard")
