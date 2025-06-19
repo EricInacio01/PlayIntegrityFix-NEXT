3fc02121b4f451b9a01375b089651be57ce15f0f
EricInacio01 <ericinacio163@gmail.com>
2025-06-19 12:34:10 -0300

    updated to v2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
-    id("com.android.application") version "8.10.1" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        println("Configurando módulo: ${com.android}")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
