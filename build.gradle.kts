55fcf955c6759837ff53ffb6ff665b760cee8ceb
EricInacio01 <ericinacio163@gmail.com>
2025-07-11 15:24:41 -0300

    update gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,58 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+tasks.register("copyZygiskFiles") {
+    doLast {
+        val moduleFolder = project.rootDir.resolve("module")
+
+        val zygiskModule = project.project(":zygisk")
+        val zygiskBuildDir = zygiskModule.layout.buildDirectory.get().asFile
+
+        val classesJar = zygiskBuildDir
+            .resolve("intermediates/dex/release/minifyReleaseWithR8/classes.dex")
+        classesJar.copyTo(moduleFolder.resolve("classes.dex"), overwrite = true)
+
+        val zygiskSoDir = zygiskBuildDir
+            .resolve("intermediates/stripped_native_libs/release/stripReleaseDebugSymbols/out/lib")
+
+        zygiskSoDir.walk()
+            .filter { it.isFile && it.name == "libzygisk.so" }
+            .forEach { soFile ->
+                val abiFolder = soFile.parentFile.name
+                val destination = moduleFolder.resolve("zygisk/$abiFolder.so")
+                soFile.copyTo(destination, overwrite = true)
+            }
     }
 }
 
-subprojects {
-    afterEvaluate {
-        println("Configurando módulo: ${com.android}")
+tasks.register("copyInjectFiles") {
+    doLast {
+        val moduleFolder = project.rootDir.resolve("module")
+
+        val injectModule = project.project(":inject")
+        val injectBuildDir = injectModule.layout.buildDirectory.get().asFile
+
+        val injectSoDir = injectBuildDir
+            .resolve("intermediates/stripped_native_libs/release/stripReleaseDebugSymbols/out/lib")
+
+        injectSoDir.walk()
+            .filter { it.isFile && it.name == "libinject.so" }
+            .forEach { soFile ->
+                val abiFolder = soFile.parentFile.name
+                val destination = moduleFolder.resolve("inject/$abiFolder.so")
+                soFile.copyTo(destination, overwrite = true)
+            }
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+tasks.register<Zip>("zip") {
+    dependsOn("copyZygiskFiles", "copyInjectFiles")
+
+    archiveFileName.set("PlayIntegrityFix.zip")
+    destinationDirectory.set(project.rootDir.resolve("out"))
+
+    from(project.rootDir.resolve("module"))
 }
