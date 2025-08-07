aef5a7bc84591c5a8769c9aa1b94d38f64634e12
EricInacio01 <ericinacio163@gmail.com>
2025-08-06 21:58:50 -0300

    update config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven("https://repo.lsposed.org/")
     }
 }
 dependencyResolutionManagement {
@@ -16,6 +17,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://repo.lsposed.org/")
     }
 }
 
